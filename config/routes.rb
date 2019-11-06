ad722f82886709f9ef250cf9450b3262a20e04fb
Benoit Baumann <baumann.benoit@gmail.com>
2019-11-06 16:14:38 +0100

    rake ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  # resources :restaurants
-  # resources :reviews
-  
   namespace :admin do
     resources :restaurants
   end
 
   resources :restaurants, only: [:index, :show, :new, :create] do 
-    resources :reviews, only: [:show, :new, :create]
+    resources :reviews, only: [:new, :create]
     end
 end
